8c1596fe00dcfdac24c493fc3f7f7d493513093c
Work <diman.marinin17@gmail.com>
2021-03-26 16:35:11 +0400

    checkpoint_from_3.26

--- double_yandex.py
+++ double_yandex.py
@@ -1,11 +1,44 @@
+# l = set()
+# for i in range(0,int(input(""))):
+# 	l.add(int(input("")))
 
-n = input("n: ")
-l = set()
-for i in range(0,int(n)):
-	num = input("")
-	l.add(num)
+# print()
+# n = len(l) / 2
 
-print("")
-print("Result")
-for i in sorted(l):
+# while len(l) >= n:
+# 	print(min(l))
+# 	l.remove(min(l))
+# for i in sorted(l):
+# 	print(i)
+
+st = list()
+for i in range(0,int(input(""))):
+	n = int(input(""))
+	if not n in st:
+		count = 0
+		for j in st:
+			if j > n:
+				st.insert(count, n)
+				count = 0
+				break
+			else:
+				count += 1
+		if count > 0 or len(st) == 0:
+			st.append(n)
+
+for i in st:
 	print(i)
+
+
+
+
+
+
+
+# s = ""
+# for i in range(0,int(input(""))):
+# 	s += " " + input("")
+# print()
+
+# for i in sorted(set(s.split())):
+# 	print(i)
--- parenthesis.py
+++ parenthesis.py
@@ -1,7 +1,18 @@
 
-def gp(n):
-	if n <= 0:
-		return ""
-	if n == 1:
-		return "()"
-	pass
+
+def generate(cur, opend, closed, n):
+	if len(cur) == 2 * n:
+		print(cur)
+		return
+	if opend < n:
+		generate(cur + "(", opend + 1, closed, n)
+	if closed < opend:
+		generate(cur + ")", opend, closed + 1, n)
+
+def parens(n):
+	generate("", 0, 0, n)
+
+num = input("N: ")
+parens(int(num))
+
+
