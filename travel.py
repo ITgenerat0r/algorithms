c72e0c10771aeb92ad5f2985786577c7894cdfcb
Work <diman.marinin17@gmail.com>
2021-03-31 16:50:30 +0400

    v3.31

--- travel.py
+++ travel.py
@@ -8,9 +8,25 @@ towns = []
 
 for it in range(0, int(input(''))):
 	a, b = input('').split()
-	towns.append([int(a), int(b)])
+	towns.append([it+1, int(a), int(b)])
 	
+fuel = int(input(""))
+a, b = input('').split()
+a = int(a)
+b = int(b)
+
+
+
 for town in towns:
-	print(town)
+	for town2 in towns:
+		if town[0] != town2[0]:
+			p = ps(town[1], town[2], town2[1], town2[2])
+			if p < fuel:
+				if not town[0]  in graph:
+					graph[town[0]] = []
+				graph[town[0]].append(town2[0])
+
 print(towns)
+print()
+print(graph[1][0])
 
