75730094178d1298b75c97bf9a66da3716adf9d5
Gen <ydalite01@gmail.com>
2022-10-29 19:41:12 +0400

    words_fast.py

--- words_fast.py
+++ words_fast.py
@@ -2,50 +2,42 @@ from colorama import Fore, Back, Style
 import colorama
 colorama.init()
 import enchant
+import itertools
 
 # dct = enchant.Dict("ru_RU")
 # dct = enchant.Dict("en_EN")
 # pwl = enchant.request_pwl_dict('dict/en_US')
-dct = enchant.DictWithPWL('ru_RU', 'dict/ru_RU')
-# dct = enchant.DictWithPWL('en_EN', 'dict/en_EN')
+# dct = enchant.DictWithPWL('ru_RU', 'dict/ru_RU')
+dct = enchant.DictWithPWL('en_EN', 'dict/en_EN')
 # set ru-RU
 # form «…\LibreOffice\share\extensions\dict-ru»
 # to C:\...\Python\Python36\site-packages\enchant\data\mingw64\share\enchant\hunspell
 print("Selected dictionary: ",dct.tag)
 
+# // Задать сразу ////////////////////////////////////////////////
+l = "й ц у a г ш щ з х ъ ф ы в д ж я б".split()
 
-l = ['t', 'e', 's', 't']
-mask = "c"
+mask = "*****"
+
+
+# // Ввести в ходе выполнения программы //////////////////////////
+
 l = []
 dt = input("  Alphabet: ")
 for i in dt:
 	l.append(i)
 
-
 mask = input(" Type mask: ")
+# ////////////////////////////////////////////////////////////////
+
+
+len_mask = len(mask)
+
 
 def gen(s, c = 0):
 	# print(c)
 	# print(s)
-	res = []
-	if len(s) == 1:
-		return s
-	if len(s) < 1:
-		return []
-	for i in s:
-		# print('i ', i)
-		g = list(s)
-		g.remove(i)
-		# print(g)
-		# print(s)
-		r = gen(g, c+1)
-		for j in r:
-			res.append(i+j)
-			if not j in res:
-				res.append(j)
-				# print(j)
-		# print()
-		# print(res)
+	res = itertools.permutations(s)
 	return res
 
 def factorial(v):
@@ -57,35 +49,41 @@ def factorial(v):
 		return v * factorial(v-1)
 
 print("Stage 1")
-len_alphabet = len(dt)
+len_alphabet = len(l)
 
 count_generate = factorial(len_alphabet)
 
 print("Generating", count_generate, "...")
 ttt = gen(l)
-print()
 
+print(ttt)
+print()
 print("Stage 2")
-rz = list()
-print("There is", len(ttt), "combinations.")
-count = 0
-for i in ttt:
-	count += 1
-	if not i in rz:
-		rz.append(i)
-	percent = 100*count/len(ttt)
-	percent = percent - (percent%0.01)
-	print(percent)
-
-print()
-print("Stage 3")
 
 f = open("words.log", 'w')
-
-for i in rz:
+count = 0
+last_i = ""
+last_percent = 0
+for ii in ttt:
+	count += 1
+	percent = 100*count/count_generate
+	percent -= percent % 0.0001
+	if last_percent != percent:
+		print(Style.RESET_ALL, percent)
+		last_percent = percent
+	i = ""
+	for it in ii:
+		i += it
+	if len_mask > 0:
+		i = i[:len_mask]
+		if i == last_i:
+			continue
+		else:
+			last_i = i
+			# input("pause for debug info")
 	k = True
-	# print(Fore.RED, mask[len(mask)-1])
-	if len(i) == len(mask) or (len(i) > len(mask)-1 and len(mask)>0 and mask[len(mask)-1]=='+'):
+	# print(Fore.RED, mask[mask_len-1])
+	if len(i) == len_mask or (len(i) > len_mask-1 and len_mask>0 and mask[len_mask-1]=='+'):
 		for j in range(0,len(mask)):
 			if mask[j] != '*':
 				if mask[j] != i[j] and mask[j] != '+':
@@ -96,7 +94,7 @@ for i in rz:
 				f.write(i + '\n')
 			else:
 				print(Fore.BLUE, i)
-	elif len(mask) == 0:
+	elif len_mask == 0:
 		if dct.check(i):
 			print(Fore.YELLOW, i)
 			f.write(i + '\n')
